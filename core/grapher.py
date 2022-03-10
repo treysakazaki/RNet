4b2a3267266e81d864f2d0d705747d0be3fe3cee
Trey Sakazaki <95950103+treysakazaki@users.noreply.github.com>
2022-03-10 15:54:34 -0800

    Update grapher.py

--- core/grapher.py
+++ core/grapher.py
@@ -1,17 +1,20 @@
+from collections import Counter, defaultdict
+import os
 
-from collections import defaultdict
 from matplotlib import pyplot as plt
 from matplotlib.offsetbox import AnchoredText
 import numpy as np
-import os
 import pandas as pd
 
 from qgis.core import (
     QgsCoordinateReferenceSystem,
     QgsCoordinateTransform,
+    QgsGeometry,
+    QgsPointXY,
     QgsProject,
     QgsTask,
-    QgsVectorLayer
+    QgsVectorLayer,
+    QgsVectorLayerUtils
     )
 
 from rnet.utils import taskmanager
@@ -25,7 +28,7 @@ passable_roads = {
     'secondary_link', 'tertiary_link', 'living_street'}
 
 
-class Graph:
+class Digraph:
     
     ident = 0
     
@@ -37,14 +40,14 @@ class Graph:
         self.crs = crs
         self.osm_ids = np.array([], dtype=int)
         self.vertices = pd.DataFrame(columns=['x', 'y'])
-        self.links = pd.DataFrame(columns=['i', 'j', 'tag'])
+        self.links = pd.DataFrame(columns=['i', 'j', 'length', 'tag'])
         self.nodes = pd.DataFrame(columns=['x', 'y'])
         self.edges = pd.DataFrame(columns=['i', 'j', 'vseq', 'length', 'tag'])
-        Graph.ident += 1
+        Digraph.ident += 1
     
     def add_data(self, src: str):
         '''
-        Add data from one or more OSM files to the Graph.
+        Add data from one or more OSM files to the Digraph.
         
         Parameters:
             src (str) -- path to OSM file
@@ -52,10 +55,10 @@ class Graph:
         assert os.path.exists(src)
         # Load OSM data
         task_name = OsmLoader.get_next_ident()
-        globals()[task_name] = OsmLoader(src)
+        globals()[task_name] = OsmLoader(src, self.crs)
         taskmanager.add_task(globals()[task_name])
         loader = globals()[task_name]
-        # Add loaded data to Graph
+        # Add loaded data to Digraph
         task_name = AddOsmData.get_next_ident()
         globals()[task_name] = AddOsmData(self, loader)
         taskmanager.add_task(globals()[task_name])
@@ -65,13 +68,14 @@ class Graph:
         if not os.path.exists(dir):
             os.mkdir(dir)
         if not overwrite:
-            names = {'vertices.csv', 'links.csv', 'nodes.csv'}
+            names = {'vertices.csv', 'links.csv', 'nodes.csv', 'edges.csv'}
             existing = os.listdir(dir)
             if not names.isdisjoint(existing):
                 raise FileExistsError(str(names.intersection(existing))[1:-1])
         self.vertices.to_csv(os.path.join(dir, 'vertices.csv'))
         self.links.to_csv(os.path.join(dir, 'links.csv'))
         self.nodes.to_csv(os.path.join(dir, 'nodes.csv'))
+        self.edges.to_csv(os.path.join(dir, 'edges.csv'))
     
     def info(self):
         print('{:14}: {:>9}'.format('name', self.name))
@@ -80,8 +84,14 @@ class Graph:
             print(f'{k:14}: {v:>9,}')
     
     def plot(self, s: int = 25):
+        # Nodes
         plt.scatter(*np.stack(self.nodes.to_numpy(), axis=1),
-                    s=s, c='#a2d418', edgecolors='k')
+                    s=s, c='#a2d418', edgecolors='k', zorder=1)
+        # Edges
+        for (i, j) in self.edges[['i', 'j']].to_numpy():
+            _, vcoords = self.vseq(i, j, return_coords=True)
+            plt.plot(*np.stack(vcoords, axis=1), color='k', zorder=0)
+        # Appearance
         plt.xlabel('X')
         plt.ylabel('Y')
         plt.tick_params(direction='in', top=True, right=True)
@@ -97,6 +107,11 @@ class Graph:
         return {k: getattr(self, k)
                 for k, v in class_items if isinstance(v, property)}
     
+    def show(self):
+        global task
+        task = Visualize(self)
+        taskmanager.add_task(task)
+    
     def transform(self, crs: int):
         ct = QgsCoordinateTransform(
                 QgsCoordinateReferenceSystem(self.crs),
@@ -110,6 +125,29 @@ class Graph:
         m = np.isin(self.vertices.index, self.nodes.index, assume_unique=True)
         self.nodes = self.vertices.loc[m]
         self.crs = crs
+    
+    def vseq(self, i: int, j: int, return_coords: bool = False):
+        '''
+        Return the vertex sequence associated with edge (i, j).
+        
+        Parameters:
+            i (int) -- node ID
+            j (int) -- node ID
+        
+        Raises:
+            KeyError if edge (i, j) does not exist.
+        
+        Return type:
+            Tuple[int]
+        '''
+        if i < j:
+            vseq = self.vseqs[(i, j)]
+        else:
+            vseq = tuple(reversed(self.vseqs[(j, i)]))
+        if not return_coords:
+            return vseq
+        else:
+            return vseq, self.vertices.loc[list(vseq)].to_numpy()
     
     @classmethod
     def get_next_ident(cls):
@@ -155,12 +193,13 @@ class OsmLoader(QgsTask):
     
     ident = 0
     
-    def __init__(self, src: str):
+    def __init__(self, src: str, crs: int):
         super().__init__('Loading OSM data')
         self.src = src
+        self.crs = crs
         self.osm_ids = np.array([], dtype=int)
         self.vertices = pd.DataFrame(columns=['x', 'y'])
-        self.links = pd.DataFrame(columns=['i', 'j', 'tag'])
+        self.links = pd.DataFrame(columns=['i', 'j', 'length', 'tag'])
         OsmLoader.ident += 1
     
     def run(self) -> bool:
@@ -198,8 +237,19 @@ class OsmLoader(QgsTask):
             vcoords = np.array(
                 [np.array([tuple(x) for x in f.geometry().asPolyline()])
                  for f in feats])
-            vertices, inverse = np.unique(
-                np.concatenate(vcoords), return_inverse=True, axis=0)
+            ind = np.concatenate([[0], np.cumsum([len(r) for r in vcoords])])
+            
+            # Transform vertex coordinates
+            if self.crs != 4326:
+                ct = QgsCoordinateTransform(
+                    QgsCoordinateReferenceSystem(4326),
+                    QgsCoordinateReferenceSystem(G.crs),
+                    QgsProject.instance())
+                vcoords = [tuple(ct.transform(x, y))
+                           for (x, y) in np.concatenate(vcoords)]
+            
+            # Remove duplicate vertices
+            vertices, inverse = np.unique(vcoords, return_inverse=True, axis=0)
             vertices = pd.DataFrame(vertices, columns=['x', 'y'])
             print(f' - {len(vertices):,} vertices', end='')
             
@@ -209,16 +259,20 @@ class OsmLoader(QgsTask):
             self.vertices = pd.concat([self.vertices, vertices])
             
             # Links
-            ind = np.concatenate([[0], np.cumsum([len(r) for r in vcoords])])
             vseqs = [inverse[ind[k]:ind[k+1]] for k in range(len(ind) - 1)]
             links = np.array(
                 [seq[k:k+2] for seq in vseqs for k in range(len(seq) - 1)])
-            i, j = np.stack(links, axis=1)
+            lengths = np.array([np.linalg.norm(np.diff(vcoords[k:k+2], axis=0))
+                                for k in range(len(vcoords) - 1)])
+            lengths = [lengths[ind[k]:ind[k+1]-1] for k in range(len(ind) - 1)]
+            lengths = np.concatenate(lengths)
             tags = [f.attribute('highway') for f in feats]
             tags = np.concatenate(
                 np.array([np.array([tags[k]] * (len(vseqs[k]) - 1))
                           for k in range(len(vseqs))]))
-            links = pd.DataFrame({'i': i, 'j': j, 'tag': tags})
+            i, j = np.stack(links, axis=1)
+            links = pd.DataFrame(
+                {'i': i, 'j': j, 'length': lengths, 'tag': tags})
             print(f' - {len(links):,} links')
             
             # Append links
@@ -259,16 +313,16 @@ class OsmLoader(QgsTask):
 
 class AddOsmData(QgsTask):
     '''
-    Task for adding data loaded from OSM file(s) to Graph model.
+    Task for adding data loaded from OSM file(s) to Digraph model.
     
     Parameters:
-        G (Graph) -- Graph instance
+        G (Digraph) -- Digraph instance
         data (OsmLoader) -- OsmLoader instance
     '''
     
     ident = 0
     
-    def __init__(self, G: Graph, data: OsmLoader):
+    def __init__(self, G: Digraph, data: OsmLoader):
         super().__init__('Adding OSM data')
         self.G = G
         self.data = data
@@ -280,18 +334,6 @@ class AddOsmData(QgsTask):
         
         # Update OSM IDs
         G.osm_ids = np.union1d(G.osm_ids, d.osm_ids)
-        
-        # Transform vertex coordinates
-        if G.crs != 4326:
-            ct = QgsCoordinateTransform(
-                QgsCoordinateReferenceSystem(4326),
-                QgsCoordinateReferenceSystem(G.crs),
-                QgsProject.instance())
-            transformed = [tuple(ct.transform(x, y))
-                           for (x, y) in d.vertices.to_numpy()]
-            x, y = np.stack(transformed, axis=1)
-            d.vertices['x'] = x
-            d.vertices['y'] = y
         
         # Append vertices
         offset = max(G.vertices.index, default=-1) + 1
@@ -319,11 +361,15 @@ class AddOsmData(QgsTask):
         G.links.drop_duplicates(inplace=True, ignore_index=True)
         print(f' - {len(G.links):,} links', end='')
         
-        # Neighbors
+        # Vertex neighbors, link lengths and tags
         G.neighbors = defaultdict(set)
-        for (i, j) in G.links[['i', 'j']].to_numpy():
+        lengths = dict()
+        tags = dict()
+        for _, i, j, length, tag in G.links.to_records():
             G.neighbors[i].add(j)
             G.neighbors[j].add(i)
+            lengths[(i, j)] = lengths[(j, i)] = length
+            tags[(i, j)] = tags[(j, i)] = tag
         
         # Extract nodes
         nodes = set(i for i, n in G.neighbors.items() if len(n) != 2)
@@ -332,7 +378,10 @@ class AddOsmData(QgsTask):
         print(f' - {len(G.nodes):,} nodes', end='')
         
         # Extract edges
-        sequences = []
+        edges = []
+        vseqs = []
+        elens = []
+        etags = []
         leaves = {min(nodes)}
         new_leaves = set()
         history = set()
@@ -349,7 +398,17 @@ class AddOsmData(QgsTask):
                         x = list(G.neighbors[q].difference({p}))[0]
                         vseq.append(x)
                         p, q = q, x
-                    sequences.append(tuple(vseq))
+                    i, j = vseq[0], vseq[-1]
+                    if i > j:
+                        i, j = j, i
+                        vseq.reverse()
+                    edges.append((i, j))
+                    vseqs.append(tuple(vseq))
+                    elens.append(np.sum([lengths[tuple(vseq[k:k+2])]
+                                         for k in range(len(vseq) - 1)]))
+                    c = Counter([tags[tuple(vseq[k:k+2])]
+                                 for k in range(len(vseq) - 1)])
+                    etags.append(c.most_common(1)[0][0])
                     history.add((q, p))
                     new_leaves.add(q)
                 visited.add(o)
@@ -357,7 +416,10 @@ class AddOsmData(QgsTask):
             leaves = new_leaves.difference(visited)
             if len(leaves) == 0 and len(nodes) > len(visited):
                 leaves = {min(nodes.difference(visited))}
-        print(f' - {len(sequences):,} edges')
+        G.vseqs = dict(zip(edges, vseqs))
+        i, j = np.stack(edges, axis=1)
+        G.edges = pd.DataFrame({'i': i, 'j': j, 'length': elens, 'tags': etags})
+        print(f' - {len(G.edges):,} edges')
         
         return True
     
@@ -368,3 +430,51 @@ class AddOsmData(QgsTask):
     @classmethod
     def get_next_ident(cls):
         return '_'.join([cls.__name__, str(cls.ident)])
+
+
+class Visualize(QgsTask):
+    
+    def __init__(self, G: Digraph):
+        super().__init__('Visualizing model')
+        self.G = G
+    
+    def run(self) -> bool:
+        G = self.G
+        
+        # Nodes
+        vl_nodes = QgsVectorLayer(
+            f'point?crs=epsg:{G.crs}', 'nodes', 'memory')
+        feats = []
+        ii, COUNT = 0, G.node_count
+        for (x, y) in G.nodes.to_numpy():
+            f = QgsVectorLayerUtils.createFeature(
+                vl_nodes, QgsGeometry.fromPointXY(QgsPointXY(x, y)))
+            feats.append(f)
+            ii += 1
+            self.setProgress(ii/COUNT*49)
+        vl_nodes.dataProvider().addFeatures(feats)
+        self.setProgress(50)
+        self.vl_nodes = vl_nodes
+        
+        # Edges
+        vl_edges = QgsVectorLayer(
+            f'linestring?crs=epsg:{G.crs}', 'edges', 'memory')
+        feats = []
+        ii, COUNT = 0, G.edge_count
+        for (i, j) in G.edges[['i', 'j']].to_numpy():
+            _, vcoords = G.vseq(i, j, return_coords=True)
+            f = QgsVectorLayerUtils.createFeature(
+                vl_edges,
+                QgsGeometry.fromPolylineXY([QgsPointXY(*p) for p in vcoords]))
+            feats.append(f)
+            ii += 1
+            self.setProgress(50 + ii/COUNT*49)
+        vl_edges.dataProvider().addFeatures(feats)
+        self.setProgress(100)
+        self.vl_edges = vl_edges
+        
+        return True
+    
+    def finished(self, success: bool):
+        QgsProject.instance().addMapLayer(self.vl_edges)
+        QgsProject.instance().addMapLayer(self.vl_nodes)
