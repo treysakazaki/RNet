45f09299bffcd6a2c07e7c3c44d30c767ba84bff
Trey Sakazaki <95950103+treysakazaki@users.noreply.github.com>
2022-04-27 22:22:32 -0700

    Create mapdata.py

---
+++ data/classes/mapdata.py
@@ -0,0 +1,50 @@
+
+from rnet.data.loaders import maploader
+from rnet.toolkits.graph import (
+    filter_connections,
+    clean_points,
+    reindex_points
+    )
+
+
+class MapData:
+    '''
+    Class for representing map data.
+    
+    Parameters:
+        vertices (pandas.DataFrame): Frame containing vertex data.
+        links (pandas.DataFrame): Frame containing link data.
+    
+    Keyword arguments:
+        crs (int): EPSG code of the CRS in which vertex coordinates are
+            represented.
+    '''
+    
+    def __init__(self, vertices, links, *, crs):
+        self.vertices = vertices
+        self.links = links
+        self.vertex_count = len(vertices)
+        self.link_count = len(links)
+    
+    @classmethod
+    def from_osm(cls, path_to_osm, **kwargs):
+        '''
+        Instantiate class from an OSM file.
+        
+        Parameters:
+            path_to_osm (str): Path to OSM file.
+    
+        Keyword arguments:
+            include (:obj:`List[str]`, optional): List of tags to include.
+            exclude (:obj:`List[str]`, optional): List of tags to exclude.
+        
+        Warning:
+            If required, either the `include` or `exclude` keyword should be
+            given, not both.
+        '''
+        vertices, links = maploader.from_osm(path_to_osm)
+        if len(kwargs) > 0:
+            links = filter_connections(links, **kwargs)
+            vertices = clean_points(vertices, links)
+            vertices, links = reindex_points(vertices, links)
+        return cls(vertices, links, crs=4326)
