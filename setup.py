13aa03124e92224e122da1aa98799557f342cf7f
Trey Sakazaki <treysakazaki@ucla.edu>
2022-05-11 16:09:07 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='RNet',
+        version='0.2.0',
+        author='Kota Sakazaki',
+        author_email='kotasakazaki@sophia.ac.jp',
+        packages=['rnet', 'rnet.data']
+        )
