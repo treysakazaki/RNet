9a8d25c31f191fdc17a617730c31ad36598293bb
Trey Sakazaki <treysakazaki@ucla.edu>
2022-05-11 13:44:45 -0700

    Update get_crs_info() function

--- rnet/toolkits/coords.py
+++ rnet/toolkits/coords.py
@@ -5,32 +5,56 @@ from scipy.spatial import cKDTree
 
 
 def get_crs_info(crs):
+    '''
+    Return information about a CRS.
+    
+    Parameters:
+        crs (int): EPSG code.
+    
+    Returns:
+        Dict[str, Any]: Dictionary mapping descriptor to value.
+    '''
     ref = osr.SpatialReference()
     err = ref.ImportFromEPSG(crs)
     if err > 0:
         return
     else:
+        area = ref.GetAreaOfUse()
+        lonmin, lonmax = area.west_lon_degree, area.east_lon_degree
+        latmin, latmax = area.south_lat_degree, area.north_lat_degree
         if ref.IsGeographic():
             reftype = 'geographic'
             units = ref.GetAngularUnitsName()
+            xmin, xmax = lonmin, lonmax
+            ymin, ymax = latmin, latmax
         elif ref.IsProjected():
             reftype = 'projected'
             units = ref.GetLinearUnitsName()
-        area = ref.GetAreaOfUse()
+            (xmin,ymin), (xmax,ymax) = transform2d(
+                np.array([(lonmin,latmin), (lonmax,latmax)]), 4326, crs)
         return {'EPSG': crs,
                 'name': ref.GetName(),
                 'type': reftype,
                 'units': units,
-                'lonmin': area.west_lon_degree,
-                'lonmax': area.east_lon_degree,
-                'latmin': area.south_lat_degree,
-                'latmax': area.north_lat_degree}
+                'lonmin': lonmin,
+                'lonmax': lonmax,
+                'latmin': latmin,
+                'latmax': latmax,
+                'xmin': xmin,
+                'xmax': xmax,
+                'ymin': ymin,
+                'ymax': ymax}
 
 
 def concatenate_points(*frames):
     '''
+    Concatenate frames containing point coordinates.
+    
     Parameters:
         *frames (pandas.DataFrame): Frames containing point coordinates.
+    
+    Returns:
+        pandas.DataFrame: Concatenated frame.
     '''
     points = []
     for df in frames:
@@ -41,18 +65,21 @@ def concatenate_points(*frames):
 
 def create_tree(points):
     '''
-    Creates two-dimensional :math:`k`-d tree for quick nearest-neighbor query.
+    Return two-dimensional :math:`k`-d tree for quick nearest-neighbor query.
     
     Parameters:
         points (:obj:`numpy.ndarray`, shape(N,2)): The N points at which
             to compute elevations.
+    
+    Returns:
+        scipy.spatial.cKDTree:
     '''
     return cKDTree(points)
 
 
 def get_bounds2d(coords):
     '''
-    Returns coordinate bounds.
+    Return coordinate bounds.
     
     Parameters:
         coords (:obj:`numpy.ndarray`, shape(N,2)): Coordinates.
@@ -67,7 +94,7 @@ def get_bounds2d(coords):
 
 def get_bounds3d(coords):
     '''
-    Returns coordinate bounds.
+    Return coordinate bounds.
     
     Parameters:
         coords (:obj:`numpy.ndarray`, shape(N,3)): Coordinates.
@@ -83,7 +110,7 @@ def get_bounds3d(coords):
 
 def get_elev(data, tree, x, y, r, p):
     '''
-    Returns elevation at a single point. The elevation is computed via
+    Return elevation at a single point. The elevation is computed via
     inverse distance weighting (IDW) interpolation.
     
     Parameters:
@@ -92,7 +119,7 @@ def get_elev(data, tree, x, y, r, p):
             query.
         x (float): `x`-coordinate.
         y (float): `y`-coordinate.
-        r (int): Radius for neighbor search.
+        r (float): Radius for neighbor search.
         p (int): Power setting for IDW interpolation.
     
     Returns:
@@ -107,7 +134,7 @@ def get_elev(data, tree, x, y, r, p):
 
 def get_elevs(data, tree, points, r, p):
     '''
-    Returns elevations at multiple points. The elevations are computed via
+    Return elevations at multiple points. The elevations are computed via
     inverse distance weighting (IDW) interpolation.
     
     Parameters:
@@ -116,7 +143,7 @@ def get_elevs(data, tree, points, r, p):
             query.
         points (:obj:`numpy.ndarray`, shape(N,2)): The N points at which
             to compute elevations.
-        r (int): Radius for neighbor search.
+        r (float): Radius for neighbor search.
         p (int): Power setting for IDW interpolation.
     
     Returns:
@@ -133,22 +160,22 @@ def get_elevs(data, tree, points, r, p):
     return res
 
 
-def transform2d(coords, srccrs, dstcrs):
+def transform2d(coords, source, destination):
     '''
-    Transforms two-dimensional coordinates.
+    Transform two-dimensional coordinates from `source` to `destination` CRS.
     
     Parameters:
         coords (:obj:`numpy.ndarray`, shape(N,2)): Coordinates to transform.
-        srccrs (int): EPSG code of source CRS.
-        dstcrs (int): EPSG code of destination CRS.
+        source (int): EPSG code of source CRS.
+        destination (int): EPSG code of destination CRS.
     
     Returns:
         :obj:`numpy.ndarray`, shape(N,2): Transformed coordinates.
     '''
     src = osr.SpatialReference()
-    src.ImportFromEPSG(srccrs)
+    src.ImportFromEPSG(source)
     dst = osr.SpatialReference()
-    dst.ImportFromEPSG(dstcrs)
+    dst.ImportFromEPSG(destination)
     ct = osr.CoordinateTransformation(src, dst)
     transformed = np.array(ct.TransformPoints(coords[:,[1,0]]))
     return transformed[:,[1,0]]
